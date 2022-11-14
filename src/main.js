6ba0f9382d55b53af1e9a9df5765cac7d5ad2cd7
Mai-Triin <110418700+Mai-Triin@users.noreply.github.com>
2022-11-14 13:55:14 +0200

    Empty commit

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import VueAxios from "vue-axios"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.js"
 
+
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
