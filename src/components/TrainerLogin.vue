608ec393920e09ebecf8164253247b8a57dd90c4
AnetteKy <anette.kytt@gmail.com>
2022-11-23 14:46:10 +0200

    Created trainer and athlete login view.

---
+++ src/components/TrainerLogin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="row justify-content-center m-3">
+      <div class="input-group">
+        <input placeholder="Kasutajanimi" type="text" class="form-control" aria-label="Sizing example input"
+               aria-describedby="inputGroup-sizing-default">
+      </div>
+    </div>
+    <div class="row justify-content-center m-3">
+      <div class="input-group">
+        <input placeholder="Parool" type="password" class="form-control" aria-label="Sizing example input"
+               aria-describedby="inputGroup-sizing-default">
+      </div>
+    </div>
+    <button class="btn btn-primary" type="submit">Logi sisse</button>
+    <div class="row justify-content-center">
+      <label class="mt-5">Ei oma veel kontot?</label>
+      <button type="button" class="btn btn-secondary btn-sm">Registreeri</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TrainerLogin'
+}
+</script>
---
+++ src/views/RoleView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+
+    <div class="container">
+      <!--      <div class="row m-5"></div>-->
+      <!--      <div class="row m-5"></div>-->
+      <!--      <div class="row m-5"></div>-->
+      <div class="row justify-content-center">
+
+        <div class="col col-lg-2 m-5">
+          <button v-on:click="displayTrainerLoginOnClick" type="button" class="btn btn-outline-primary">TREENER</button>
+
+          <TrainerLogin v-if="displayTrainerLogin"/>
+
+        </div>
+
+        <div class="col col-lg-2 m-5">
+          <button v-on:click="displayAthleteLoginOnClick" type="button" class="btn btn-outline-success">TREENITAV</button>
+
+          <AthleteLogin v-if="displayAthleteLogin"/>
+
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import TrainerLogin from "@/components/TrainerLogin";
+import AthleteLogin from "@/components/AthleteLogin";
+
+export default {
+  name: 'HomeView',
+  components: {AthleteLogin, TrainerLogin},
+
+  data: function () {
+    return {
+      displayTrainerLogin: false,
+      displayAthleteLogin: false
+    }
+  },
+
+  methods: {
+    displayTrainerLoginOnClick: function () {
+      this.displayTrainerLogin = true
+    },
+    displayAthleteLoginOnClick: function () {
+      this.displayAthleteLogin = true
+    }
+  }
+
+}
+
+
+</script>
