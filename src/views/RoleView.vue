381554721d659b06e4f79e772603b313b6b73112
Mai-Triin <110418700+Mai-Triin@users.noreply.github.com>
2022-11-24 13:34:53 +0200

    MAI - commit

--- src/views/RoleView.vue
+++ src/views/RoleView.vue
@@ -1,23 +1,33 @@
 <template>
+<!--  <div>-->
+<!--    <div class="row align-items-start">-->
+<!--      <img src="../assets/logo.png">-->
+<!--    </div>-->
+
     <div class="container d-flex justify-content-around align-items-center">
-            <div class="row d-flex flex-grow-1">
+      <div class="row d-flex flex-grow-1">
 
-              <div class="col m-5">
-                <button v-on:click="displayTrainerLoginOnClick" type="button" class="btn btn-outline-primary btn-lg">TREENER</button>
 
-                <TrainerLogin v-if="displayTrainerLogin"/>
+        <div class="col m-5">
+          <button v-on:click="displayTrainerLoginOnClick" type="button" class="btn btn-outline-primary btn-lg">TREENER
+          </button>
 
-              </div>
+          <TrainerLogin v-if="displayTrainerLogin"/>
 
-              <div class="col m-5">
-                <button v-on:click="displayAthleteLoginOnClick" type="button" class="btn btn-outline-success btn-lg">TREENIJA</button>
+        </div>
 
-                <AthleteLogin v-if="displayAthleteLogin"/>
+        <div class="col m-5">
+          <button v-on:click="displayAthleteLoginOnClick" type="button" class="btn btn-outline-success btn-lg">
+            TREENIJA
+          </button>
 
-              </div>
+          <AthleteLogin v-if="displayAthleteLogin"/>
 
-            </div>
+        </div>
+
+      </div>
     </div>
+<!--  </div>-->
 </template>
 
 <script>
