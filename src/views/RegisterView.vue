beac1124838b6737c440e267f60fef126a3a39fc
Mihke <mihkelviirsalu@hotmail.com>
2022-11-28 14:40:30 +0200

    JOM-5 - Register view created, roletype saves in sessionstorage

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -34,13 +34,117 @@
         </label>
       </div>
 
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">E-mail</span>
+        <input type="text" class="form-control" aria-label="Sizing example input"
+               aria-describedby="inputGroup-sizing-default">
+      </div>
+
+      <div>
+        <select class="form-select mb-3" aria-label="Default select example">
+          <option v-model:selected="selectedRoleType"></option>
+          <option v-for="role in roleTypes" :key="role.roleId" :value="role.roleId">{{ role.roleType }}</option>
+
+        </select>
+      </div>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Kasutajanimi</span>
+        <input type="text" class="form-control" aria-label="Sizing example input"
+               aria-describedby="inputGroup-sizing-default">
+      </div>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Parool</span>
+        <input type="text" class="form-control" aria-label="Sizing example input"
+               aria-describedby="inputGroup-sizing-default">
+      </div>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Parool uuesti</span>
+        <input type="text" class="form-control" aria-label="Sizing example input"
+               aria-describedby="inputGroup-sizing-default">
+      </div>
+
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+          Olen kasutajatingimustega nõus
+        </label>
+      </div>
+
+      <div>
+        <button v-on:click="registerUser" class="btn btn-success" type="submit">Registreeri</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "RegisterView"
+  name: "RegisterView",
+
+  data: function () {
+    return {
+
+      // athlete: sessionStorage.getItem('athlete'),
+      // trainer: sessionStorage.getItem('trainer'),
+
+
+      roleTypes: [
+        {
+          roleId: 0,
+          roleType: ''
+        }
+      ],
+
+      userRequest: {
+        firstName: '',
+        lastName: '',
+        birthDate: '',
+        sex: '',
+        email: '',
+        termsAccept: '',
+        loginInfo: {
+          username: '',
+          password: '',
+        }
+      },
+      registerResponse: {
+        userId: 0,
+        roleId: 0,
+        roleType: ''
+      }
+    }
+  },
+  methods: {
+
+    selectedRoleType: function () {
+      if (sessionStorage.getItem('athlete')) {
+        return this.athlete;
+      } else {
+        return this.trainer
+      }
+
+    },
+
+    registerUser: function () {
+
+      if (this.registerResponse.roleType === 'Treener') {
+        this.$router.push({name: 'trainerHomeRoute'});
+      } else {
+        this.$router.push({name: 'athleteHomeRoute'})
+
+      }
+
+      this.$http.post("/register", this.userRequest
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+  }
 }
 </script>
 
--- src/views/RoleView.vue
+++ src/views/RoleView.vue
@@ -1,33 +1,33 @@
 <template>
-<!--  <div>-->
-<!--    <div class="row align-items-start">-->
-<!--      <img src="../assets/logo.png">-->
-<!--    </div>-->
+  <!--  <div>-->
+  <!--    <div class="row align-items-start">-->
+  <!--      <img src="../assets/logo.png">-->
+  <!--    </div>-->
 
-    <div class="container d-flex justify-content-around align-items-center">
-      <div class="row d-flex flex-grow-1">
+  <div class="container d-flex justify-content-around align-items-center">
+    <div class="row d-flex flex-grow-1">
 
 
-        <div class="col m-5">
-          <button v-on:click="displayTrainerLoginOnClick" type="button" class="btn btn-outline-primary btn-lg">TREENER
-          </button>
+      <div class="col m-5">
+        <button v-on:click="displayTrainerLoginOnClick" type="button" class="btn btn-outline-primary btn-lg">TREENER
+        </button>
 
-          <TrainerLogin v-if="displayTrainerLogin"/>
-
-        </div>
-
-        <div class="col m-5">
-          <button v-on:click="displayAthleteLoginOnClick" type="button" class="btn btn-outline-success btn-lg">
-            TREENIJA
-          </button>
-
-          <AthleteLogin v-if="displayAthleteLogin"/>
-
-        </div>
+        <TrainerLogin v-if="displayTrainerLogin"/>
 
       </div>
+
+      <div class="col m-5">
+        <button v-on:click="displayAthleteLoginOnClick" type="button" class="btn btn-outline-success btn-lg">
+          TREENIJA
+        </button>
+
+        <AthleteLogin v-if="displayAthleteLogin"/>
+
+      </div>
+
     </div>
-<!--  </div>-->
+  </div>
+  <!--  </div>-->
 </template>
 
 <script>
@@ -43,17 +43,21 @@ export default {
   data: function () {
     return {
       displayTrainerLogin: false,
-      displayAthleteLogin: false
+      displayAthleteLogin: false,
+      athlete: 'Treenija',
+      trainer: 'Treener'
     }
   },
 
   methods: {
     displayTrainerLoginOnClick: function () {
       this.displayTrainerLogin = true
+      sessionStorage.setItem('trainer', this.trainer)
 
     },
     displayAthleteLoginOnClick: function () {
       this.displayAthleteLogin = true
+      sessionStorage.setItem('athlete', this.athlete)
     }
   }
 
