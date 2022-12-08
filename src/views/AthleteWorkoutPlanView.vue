4431052d8509519182806b6aba3e8663f98d2930
AnetteKy <anette.kytt@gmail.com>
2022-12-08 09:38:32 +0200

    JOM-7-anette

--- src/views/AthleteWorkoutPlanView.vue
+++ src/views/AthleteWorkoutPlanView.vue
@@ -1,64 +1,79 @@
 <template>
-
-  <div class="motherFlex flex-column ">
-
+  <div class="motherFlex2 flex-column">
     <AthleteNavBar/>
 
-    <div class="row justify-content-around m-1">
-      <div class="col-md-2">
-        <h5>Minu treeningkavad</h5>
-        <!--          <table class="table table-hover">-->
-        <!--            <thead>-->
-        <!--            <tr>-->
-        <!--              <th scope="col">Minu treeningkavad</th>-->
-        <!--            </tr>-->
-        <!--            </thead>-->
-        <!--            <tbody>-->
-        <!--            <tr>-->
-        <!--              <td>Mingi trenn 1</td>-->
-        <!--            </tr>-->
-        <!--            <tr>-->
-        <!--              <td>Mingi trenn 2</td>-->
-        <!--            </tr>-->
-        <!--            <tr>-->
-        <!--              <td colspan="2">Mingi trenn 3</td>-->
-        <!--            </tr>-->
-        <!--            </tbody>-->
-        <!--          </table>-->
+    <div class="container">
+      <div class="col">
+        jnsvkj
       </div>
-      <div class="d-md-flex justify-content-md-end">
-        <button v-on:click="navigateToExerciseView" type="button" class="btn btn-success btn-lg m-5">+ Lisa
-          treeningkava
-        </button>
+      <div class="col">
+        fffvdf
       </div>
     </div>
 
-    <div class="row justify-content-center m-3">
-      <div class="col-6 justify-content-center">
-        <h2 class="col col-4 m-3">Minu treeningkavad</h2>
-        <h5 class="col col-4 m-1">(treeningkava nimi)</h5>
-
-        <!--          <button v-on:click="navigateToExerciseView"-->
-        <!--                  type="button" class="col col-1 m-1 btn btn-success">Muuda</button>-->
-        <!--          <button type="button" class="col col-1 m-1 btn btn-success">Kustuta</button>-->
-
-        <AthleteWorkoutPlanTable/>
-
-        <div class="row col-2 offset-10">
-          <button type="button" class="btn btn-success ">Soorita kava</button>
-        </div>
-        <div class="row col-2 offset-10">
-          <button type="button" class="btn btn-success ">Muuda kava</button>
-        </div>
-        <div class="row col-2 offset-10">
-          <button type="button" class="btn btn-success ">Kustuta kava</button>
-        </div>
-
-      </div>
-
-
-    </div>
   </div>
+
+
+
+<!--  <div class="motherFlex flex-column ">-->
+
+<!--    <AthleteNavBar/>-->
+
+<!--    <div class="row justify-content-around m-1">-->
+<!--      <div class="col-md-2">-->
+<!--        <h5>Minu treeningkavad</h5>-->
+<!--                  <table class="table table-hover">-->
+<!--                    <thead>-->
+<!--                    <tr>-->
+<!--                      <th scope="col">Minu treeningkavad</th>-->
+<!--                    </tr>-->
+<!--                    </thead>-->
+<!--                    <tbody>-->
+<!--                    <tr>-->
+<!--                      <td>Mingi trenn 1</td>-->
+<!--                    </tr>-->
+<!--                    <tr>-->
+<!--                      <td>Mingi trenn 2</td>-->
+<!--                    </tr>-->
+<!--                    <tr>-->
+<!--                      <td colspan="2">Mingi trenn 3</td>-->
+<!--                    </tr>-->
+<!--                    </tbody>-->
+<!--                  </table>-->
+<!--      </div>-->
+<!--      <div class="d-md-flex justify-content-md-end">-->
+<!--        <button v-on:click="navigateToExerciseView" type="button" class="btn btn-success btn-lg m-5">+ Lisa-->
+<!--          treeningkava-->
+<!--        </button>-->
+<!--      </div>-->
+<!--    </div>-->
+
+<!--    <div class="row justify-content-center m-3">-->
+<!--      <div class="col-6 justify-content-center">-->
+<!--        <h2 class="col col-4 m-3">Minu treeningkavad</h2>-->
+<!--        <h5 class="col col-4 m-1">(treeningkava nimi)</h5>-->
+
+<!--        &lt;!&ndash;          <button v-on:click="navigateToExerciseView"&ndash;&gt;-->
+<!--        &lt;!&ndash;                  type="button" class="col col-1 m-1 btn btn-success">Muuda</button>&ndash;&gt;-->
+<!--        &lt;!&ndash;          <button type="button" class="col col-1 m-1 btn btn-success">Kustuta</button>&ndash;&gt;-->
+
+<!--        <AthleteWorkoutPlanTable/>-->
+
+<!--        <div class="row col-2 offset-10">-->
+<!--          <button type="button" class="btn btn-success ">Soorita kava</button>-->
+<!--        </div>-->
+<!--        <div class="row col-2 offset-10">-->
+<!--          <button type="button" class="btn btn-success ">Muuda kava</button>-->
+<!--        </div>-->
+<!--        <div class="row col-2 offset-10">-->
+<!--          <button type="button" class="btn btn-success ">Kustuta kava</button>-->
+<!--        </div>-->
+
+<!--      </div>-->
+
+
+<!--    </div>-->
+<!--  </div>-->
 
 </template>
 
